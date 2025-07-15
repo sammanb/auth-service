5e8f8cd13a32631296dbd05f9bc0feb51880d2ee
Samman Banerjee <arranger3000@gmail.com>
2025-07-16 01:45:16 +0530

    correction in tenant model

--- internal/models/tenant.go
+++ internal/models/tenant.go
@@ -7,9 +7,8 @@ import (
 )
 
 type Tenant struct {
-	ID   string `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	ID   string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
 	Name string `gorm:"not null" json:"name"`
-	gorm.Model
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
