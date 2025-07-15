709a64ab63f4e89f50b03704c67da6b36598aabd
Samman Banerjee <arranger3000@gmail.com>
2025-07-16 01:39:51 +0530

    added preinit to create database if not exists

--- config/db.go
+++ config/db.go
@@ -1,12 +1,15 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	_ "github.com/lib/pq"
 )
 
 var DB *gorm.DB
@@ -23,18 +26,52 @@ func loadEnv() error {
 	return nil
 }
 
+func PreInit() string {
+	// dsn := "host=localhost user=postgres password=yourpass dbname=postgres sslmode=disable"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
+		viper.GetString("DB_HOST"),
+		viper.GetString("DB_USER"),
+		viper.GetString("DB_PASSWORD"),
+		viper.GetString("DB_PORT"),
+	)
+	adminDB, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("postgres db not found!")
+	}
+
+	dbName := viper.GetString("DB_NAME")
+	var exists bool
+	err = adminDB.QueryRow("SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname = $1)", dbName).Scan(&exists)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !exists {
+		_, err := adminDB.Exec("CREATE DATABASE " + dbName)
+		if err != nil {
+			log.Fatal("failed to create database. ", err)
+		}
+	}
+
+	log.Println("database created successfully")
+	return dbName
+}
+
 func InitDB() *gorm.DB {
 	err := loadEnv()
 	if err != nil {
 		log.Fatal("failed to read env")
 	}
 
+	dbName := PreInit()
+
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		viper.GetString("DB_HOST"),
 		viper.GetString("DB_USER"),
 		viper.GetString("DB_PASSWORD"),
-		viper.GetString("DB_NAME"),
+		dbName,
 		viper.GetString("DB_PORT"),
 	)
 
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	go.mongodb.org/mongo-driver v1.17.4
