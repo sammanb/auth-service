32af4d35b905880d6070850ce0f079ffbbd3114c
Samman Banerjee <arranger3000@gmail.com>
2025-07-16 23:20:41 +0530

    added is_default to role model

--- internal/models/role.go
+++ internal/models/role.go
@@ -13,6 +13,7 @@ type Role struct {
 	ID          uuid.UUID     `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
 	Name        string        `gorm:"uniqueIndex;not null" json:"name"`
 	Permissions []*Permission `gorm:"many2many:role_permissions" json:"permissions"`
+	IsDefault   bool          `gorm:"default:false" json:"is_default"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
