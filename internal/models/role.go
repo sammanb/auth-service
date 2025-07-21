97f952776b257cfa5c5ae117ccb680ed5d48669b
Samman Banerjee <arranger3000@gmail.com>
2025-07-22 01:51:12 +0530

    added tenant_id to role model

--- internal/models/role.go
+++ internal/models/role.go
@@ -11,6 +11,7 @@ var RoleSuperAdmin = "superadmin"
 
 type Role struct {
 	ID          uuid.UUID     `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	TenantID    *uuid.UUID    `gorm:"type:uuid" json:"tenant_id"`
 	Name        string        `gorm:"uniqueIndex;not null" json:"name"`
 	Permissions []*Permission `gorm:"many2many:role_permissions" json:"permissions"`
 	IsDefault   bool          `gorm:"default:false" json:"is_default"`
