697d07cdca04274b3a3469a653e10b8e34b091be
Samman Banerjee <arranger3000@gmail.com>
2025-07-23 12:41:19 +0530

    composite unique key set in permission

--- internal/models/permission.go
+++ internal/models/permission.go
@@ -8,10 +8,11 @@ import (
 )
 
 type Permission struct {
-	ID       uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-	Action   string    `gorm:"not null" json:"action"`   // CRUD
-	Resource string    `gorm:"not null" json:"resource"` // e.g. user, workspace, file
-	Code     string    `gorm:"uniqueIndex;" json:"code"`
+	ID       uuid.UUID  `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	TenantID *uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_tenant_code" json:"tenant_id"`
+	Action   string     `gorm:"not null" json:"action"`   // CRUD
+	Resource string     `gorm:"not null" json:"resource"` // e.g. user, workspace, file
+	Code     string     `gorm:"not null;uniqueIndex:idx_tenant_code" json:"code"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
