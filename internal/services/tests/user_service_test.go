5f0999cf50952f3c12f18556bca9bd7f05a381d1
Samman Banerjee <arranger3000@gmail.com>
2025-07-24 12:43:29 +0530

    user service tests

--- internal/services/tests/user_service_test.go
+++ internal/services/tests/user_service_test.go
@@ -41,8 +41,8 @@ func TestFindUserByEmail_Failure(t *testing.T) {
 	mockUserRepo := &mocks.MockUserRepository{}
 	mockRoleRepo := &mocks.MockRoleRepository{}
 	mockPermissionRepo := &mocks.MockPermissionRepository{}
-	authService := &mocks.MockAuthService{}
-	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, authService)
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
 
 	email := "testuser@mail.com"
 
@@ -61,8 +61,8 @@ func TestCreateUser_Success(t *testing.T) {
 	mockUserRepo := &mocks.MockUserRepository{}
 	mockRoleRepo := &mocks.MockRoleRepository{}
 	mockPermissionRepo := &mocks.MockPermissionRepository{}
-	authService := &mocks.MockAuthService{}
-	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, authService)
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
 
 	userID := uuid.New()
 	tenantID := uuid.New()
@@ -114,8 +114,8 @@ func TestCreateUser_Failure(t *testing.T) {
 	mockUserRepo := &mocks.MockUserRepository{}
 	mockRoleRepo := &mocks.MockRoleRepository{}
 	mockPermissionRepo := &mocks.MockPermissionRepository{}
-	authService := &mocks.MockAuthService{}
-	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, authService)
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
 
 	userID := uuid.New()
 	tenantID := uuid.New()
@@ -160,11 +160,11 @@ func TestCreateUser_Failure(t *testing.T) {
 }
 
 func TestLogin_Success(t *testing.T) {
-	userRepo := &mocks.MockUserRepository{}
-	permissionRepo := &mocks.MockPermissionRepository{}
-	roleRepo := &mocks.MockRoleRepository{}
-	authService := &mocks.MockAuthService{}
-	userService := services.NewUserService(userRepo, roleRepo, permissionRepo, authService)
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
 
 	email := "testuser@mail.com"
 	password := "password"
@@ -175,14 +175,331 @@ func TestLogin_Success(t *testing.T) {
 
 	token := "123"
 
-	userRepo.On("FindUserByEmail", email).Return(expectedUser, nil)
-	authService.On("CompareHashAndPassword", []byte(password), []byte(expectedUser.PasswordHash)).Return(true)
-	authService.On("GenerateJWT", mock.Anything).Return(token, nil)
+	mockUserRepo.On("FindUserByEmail", email).Return(expectedUser, nil)
+	mockAuthService.On("CompareHashAndPassword", []byte(password), []byte(expectedUser.PasswordHash)).Return(true)
+	mockAuthService.On("GenerateJWT", mock.Anything).Return(token, nil)
 
 	result, err := userService.Login(email, password)
 
 	assert.NoError(t, err)
 	require.NotEmpty(t, result)
-	authService.AssertCalled(t, "GenerateJWT", expectedUser)
-	authService.AssertExpectations(t)
+	mockAuthService.AssertCalled(t, "GenerateJWT", expectedUser)
+	mockAuthService.AssertExpectations(t)
+}
+
+func TestRemoveUserById_Success(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenant_id := "tenant_id"
+	user_id := "user_id"
+	mockUserRepo.On("RemoveUserById", tenant_id, user_id).Return(nil)
+
+	err := userService.RemoveUserById(tenant_id, user_id)
+
+	assert.Nil(t, err)
+	mockUserRepo.AssertCalled(t, "RemoveUserById", tenant_id, user_id)
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestRemoveUserById_Failure(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenant_id := "tenant_id"
+	user_id := "user_id"
+	mockUserRepo.On("RemoveUserById", tenant_id, user_id).Return(errors.New("user not found"))
+
+	err := userService.RemoveUserById(tenant_id, user_id)
+
+	assert.NotNil(t, err)
+	mockUserRepo.AssertCalled(t, "RemoveUserById", tenant_id, user_id)
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestInitResetPassword_Success(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	email := "testuser@mail.com"
+	userId := uuid.New()
+
+	expectedUser := &models.User{
+		ID:    userId,
+		Email: email,
+	}
+
+	mockUserRepo.On("FindUserByEmail", email).Return(expectedUser, nil)
+
+	rawToken := "rawToken"
+	hashedToken := "hashedToken"
+	utils.CreateRandomToken = func() (string, string, error) {
+		return rawToken, hashedToken, nil
+	}
+	defer func() {
+		utils.CreateRandomToken = utils.GenerateRandomToken
+	}()
+
+	mockUserRepo.On("SetResetPasswordTokenHash", userId.String(), hashedToken).Return(nil)
+
+	token, err := userService.InitResetPassword(email)
+
+	assert.Nil(t, err)
+	assert.Equal(t, rawToken, token)
+	mockUserRepo.AssertCalled(t, "SetResetPasswordTokenHash", userId.String(), hashedToken)
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestInitResetPassword_Failure(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	email := "testuser@mail.com"
+	userId := uuid.New()
+
+	expectedUser := &models.User{
+		ID:    userId,
+		Email: email,
+	}
+
+	mockUserRepo.On("FindUserByEmail", email).Return(expectedUser, nil)
+
+	rawToken := "rawToken"
+	hashedToken := "hashedToken"
+	utils.CreateRandomToken = func() (string, string, error) {
+		return rawToken, hashedToken, nil
+	}
+	defer func() {
+		utils.CreateRandomToken = utils.GenerateRandomToken
+	}()
+
+	mockUserRepo.On("SetResetPasswordTokenHash", userId.String(), hashedToken).Return(errors.New("error"))
+
+	token, err := userService.InitResetPassword(email)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "", token)
+	mockUserRepo.AssertCalled(t, "SetResetPasswordTokenHash", userId.String(), hashedToken)
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestResetPassword_Success(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+	token := "token"
+	newPassword := "newPassword"
+	resetPasswordTokenHash := "resetPasswordTokenHash"
+
+	user := &models.User{
+		ID:                     userId,
+		TenantID:               &tenantId,
+		PasswordHash:           "hashedPassword",
+		ResetPasswordTokenHash: resetPasswordTokenHash,
+	}
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(user, nil)
+	mockAuthService.On("CompareHashAndPassword", []byte(token), []byte(user.ResetPasswordTokenHash)).Return(true)
+	mockAuthService.On("HashPassword", newPassword).Return("hashedPassword", nil)
+	mockUserRepo.On("UpdateUser", user).Return(nil)
+
+	err := userService.ResetPassword(tenantId.String(), userId.String(), token, newPassword)
+
+	assert.Nil(t, err)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+	mockAuthService.AssertCalled(t, "CompareHashAndPassword", []byte(token), []byte(resetPasswordTokenHash))
+	mockAuthService.AssertCalled(t, "HashPassword", newPassword)
+	mockUserRepo.AssertCalled(t, "UpdateUser", user)
+
+	mockUserRepo.AssertExpectations(t)
+	mockAuthService.AssertExpectations(t)
+}
+
+func TestResetPassword_InvalidUser_Failed(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+	token := "token"
+	newPassword := "newPassword"
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(nil, errors.New("user not found"))
+
+	err := userService.ResetPassword(tenantId.String(), userId.String(), token, newPassword)
+
+	assert.NotNil(t, err)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestResetPassword_InvalidToken_Failed(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+	token := "token"
+	newPassword := "newPassword"
+	resetPasswordTokenHash := "resetPasswordTokenHash"
+
+	user := &models.User{
+		ID:                     userId,
+		TenantID:               &tenantId,
+		PasswordHash:           "hashedPassword",
+		ResetPasswordTokenHash: resetPasswordTokenHash,
+	}
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(user, nil)
+	mockAuthService.On("CompareHashAndPassword", []byte(token), []byte(user.ResetPasswordTokenHash)).Return(false)
+
+	err := userService.ResetPassword(tenantId.String(), userId.String(), token, newPassword)
+
+	assert.NotNil(t, err)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+	mockAuthService.AssertCalled(t, "CompareHashAndPassword", []byte(token), []byte(resetPasswordTokenHash))
+
+	mockUserRepo.AssertExpectations(t)
+	mockAuthService.AssertExpectations(t)
+}
+
+func TestResetPassword_UpdateUser_Failure(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+	token := "token"
+	newPassword := "newPassword"
+	resetPasswordTokenHash := "resetPasswordTokenHash"
+
+	user := &models.User{
+		ID:                     userId,
+		TenantID:               &tenantId,
+		PasswordHash:           "hashedPassword",
+		ResetPasswordTokenHash: resetPasswordTokenHash,
+	}
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(user, nil)
+	mockAuthService.On("CompareHashAndPassword", []byte(token), []byte(user.ResetPasswordTokenHash)).Return(true)
+	mockAuthService.On("HashPassword", newPassword).Return("hashedPassword", nil)
+	mockUserRepo.On("UpdateUser", user).Return(errors.New("user update failed"))
+
+	err := userService.ResetPassword(tenantId.String(), userId.String(), token, newPassword)
+
+	assert.NotNil(t, err)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+	mockAuthService.AssertCalled(t, "CompareHashAndPassword", []byte(token), []byte(resetPasswordTokenHash))
+	mockAuthService.AssertCalled(t, "HashPassword", newPassword)
+	mockUserRepo.AssertCalled(t, "UpdateUser", user)
+
+	mockUserRepo.AssertExpectations(t)
+	mockAuthService.AssertExpectations(t)
+}
+
+func TestGetUsers_Success(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+
+	expectedUser := &models.User{
+		ID:       userId,
+		TenantID: &tenantId,
+	}
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(expectedUser, nil)
+
+	user, err := userService.GetUserById(tenantId.String(), userId.String())
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedUser.ID, user.ID)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+}
+
+func TestGetUsers_Failuer(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(nil, errors.New("user not found"))
+
+	user, err := userService.GetUserById(tenantId.String(), userId.String())
+
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+}
+
+func TestUpdateUserRole_Success(t *testing.T) {
+	mockUserRepo := &mocks.MockUserRepository{}
+	mockPermissionRepo := &mocks.MockPermissionRepository{}
+	mockRoleRepo := &mocks.MockRoleRepository{}
+	mockAuthService := &mocks.MockAuthService{}
+	userService := services.NewUserService(mockUserRepo, mockRoleRepo, mockPermissionRepo, mockAuthService)
+
+	tenantId := uuid.New()
+	userId := uuid.New()
+	roleName := "roleName"
+
+	expectedRole := &models.Role{
+		ID:       uuid.New(),
+		TenantID: &tenantId,
+	}
+
+	user := &models.User{
+		ID:       userId,
+		TenantID: &tenantId,
+		RoleID:   expectedRole.ID.String(),
+		Role:     *expectedRole,
+	}
+
+	mockRoleRepo.On("GetRoleByName", tenantId.String(), roleName).Return(expectedRole, nil)
+	mockUserRepo.On("GetUserById", tenantId.String(), userId.String()).Return(user, nil)
+	mockUserRepo.On("UpdateUser", user).Return(nil)
+
+	err := userService.UpdateUserRole(tenantId.String(), userId.String(), roleName)
+
+	assert.Nil(t, err)
+	mockRoleRepo.AssertCalled(t, "GetRoleByName", tenantId.String(), roleName)
+	mockUserRepo.AssertCalled(t, "GetUserById", tenantId.String(), userId.String())
+	mockUserRepo.AssertCalled(t, "UpdateUser", user)
+
+	mockRoleRepo.AssertExpectations(t)
+	mockUserRepo.AssertExpectations(t)
 }
